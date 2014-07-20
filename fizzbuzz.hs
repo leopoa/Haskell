8e3e9008add82144a5ceb7f0fd0ae3aa05cc0daa
leopoa <leo.poa@gmail.com>
2014-07-20 12:25:24 -0300

    example fizzbuzz

---
+++ fizzbuzz.hs
@@ -0,0 +1,16 @@
+
+
+fizzBuzz :: Int -> Bool
+fizzBuzz number = number `mod` 5 == 0  && number `mod` 3 == 0
+
+fizz :: Int -> Bool
+fizz number = number `mod` 3 == 0
+
+buzz :: Int -> Bool
+buzz number = number `mod` 5 == 0 
+
+
+fb = [ if fizzBuzz x then "FIZZBUZZ" else if fizz x then "Fizz" else if buzz x then "Buzz" else show x | x <- [1..20] ] 
+
+-- versao sem refatoração
+-- fb = [ if x `mod` 5 == 0 && x `mod` 3 == 0 then "FIZZBUZZ" else if x `mod` 5 == 0 then "Buzz" else if x `mod` 3 == 0 then "FIZZ" else show x | x <- [1..20] ] 
