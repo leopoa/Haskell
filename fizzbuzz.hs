2b05cae93d7ef3367babb3b46cb848d92d61f0c4
leopoa <leo.poa@gmail.com>
2014-07-20 12:35:48 -0300

    refactor

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,13 +1,11 @@
-
-
-fizzBuzz :: Int -> Bool
-fizzBuzz number = number `mod` 5 == 0  && number `mod` 3 == 0
-
 fizz :: Int -> Bool
 fizz number = number `mod` 3 == 0
 
 buzz :: Int -> Bool
 buzz number = number `mod` 5 == 0 
+
+fizzBuzz :: Int -> Bool
+fizzBuzz number = fizz number && buzz number
 
 
 fb = [ if fizzBuzz x then "FIZZBUZZ" else if fizz x then "Fizz" else if buzz x then "Buzz" else show x | x <- [1..20] ] 
